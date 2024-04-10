51f0f15feae3becfc695dc2d1f0995b380702647
Fabiana Pontes <fabianapontesevaristo@gmail.com>
2024-04-10 20:48:03 -0300

    acrecentou o link home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; 
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faHome } from "@fortawesome/free-solid-svg-icons";
 
 
 function Header({quantidade}) {
   return(
     <div className="header">
       <div className="container">
+        <a  href={`/`}><FontAwesomeIcon icon={faHome} style={{float: "left"}}/></a>
         <a  href={`/carrinho`}>
           <FontAwesomeIcon icon={faCartShopping} /> 
           <span className='quantidade'>{quantidade}</span>
